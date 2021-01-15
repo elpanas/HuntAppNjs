3a1fb9de9bd0eb7b45d555aa70e4cdde80802ef6
elpanas <luca.panariello@gmail.com>
2021-01-15 17:38:20 +0100

    corrections

--- middleware/pdfware.js
+++ middleware/pdfware.js
@@ -23,9 +23,9 @@ async function generateQrPdf(idg) {
         qrfilename,
         pathinputs = [];
         
-    if(!fs.existsSync(tmpqrc)) fs.mkdir(tmpqrc, {recursive: true});
-    if(!fs.existsSync(tmppdf)) fs.mkdir(tmppdf, {recursive: true});
-    if(!fs.existsSync(dirpdf)) fs.mkdir(dirpdf, {recursive: true});    
+    if(!fs.existsSync(tmpqrc)) fs.mkdir(tmpqrc, {recursive: true}, err => {});
+    if(!fs.existsSync(tmppdf)) fs.mkdir(tmppdf, {recursive: true}, err => {});
+    if(!fs.existsSync(dirpdf)) fs.mkdir(dirpdf, {recursive: true}, err => {});    
 
     const locations = await getLocations(idg);
 
