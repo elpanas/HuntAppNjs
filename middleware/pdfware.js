88c354edd9a4c7ad6e9330ff35accf3bf71755bd
elpanas <luca.panariello@gmail.com>
2021-01-16 16:08:02 +0100

    pdf corrections

--- middleware/pdfware.js
+++ middleware/pdfware.js
@@ -60,7 +60,7 @@ async function generateQrPdf(idg) {
         .inputs(pathinputs)
         .option('page-size', 'A4')
         .output(dirpdf + idg + '-file.pdf')    
-        .binary(process.cwd() + '/bin/prince')            
+        //.binary(process.cwd() + '/bin/prince')            
         .execute()
         .then(() => {
             console.log("OK: done");
